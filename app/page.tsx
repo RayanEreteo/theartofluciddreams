503244c679d27c5b26ad16602af96713c022d861
RayanEreteo <rayabf5@gmail.com>
2023-12-15 16:49:01 +0100

    page d'intro terminée

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,13 @@ export default function Home() {
       className="Main"
       backgroundColor={"#7FC6B8"}
       flexDirection={"column"}
-      textAlign={"center"}
       justifyContent={"center"}
+      alignItems={"center"}
       m={"auto"}
       w={"100vw"}
       h={"100vh"}
     >
-      <Heading fontSize={"4rem"} color={"white"}>
+      <Heading fontSize={"4rem"} color={"white"} textAlign={"center"}>
         L'art des rêves lucides
       </Heading>
       <Text fontSize={"1.3rem"} opacity={"0.8"} color={"white"}>
@@ -23,9 +23,10 @@ export default function Home() {
           Rayan Ereteo
         </Text>
       </Text>
-      <Box className="luciddream-desc">
+      <Box className="luciddream-desc" color={"white"} mt={"10rem"} textAlign={"center"}>
         <Heading>Un rêve lucide, c'est quoi ?</Heading>
-        <Text>
+        <br />
+        <Text maxW={"800px"} fontSize={"1.2rem"}>
           Un rêve lucide est un type particulier de rêve où le rêveur devient
           conscient qu'il est en train de rêver. Dans un rêve lucide, la
           personne prend conscience de son état de rêve tout en restant
@@ -34,7 +35,7 @@ export default function Home() {
           propre rêve.
         </Text>
       </Box>
-      <Flex flexDirection={"row"}></Flex>
+      <Flex className="category-wrapper" flexDirection={"row"}></Flex>
     </Flex>
   );
 }
